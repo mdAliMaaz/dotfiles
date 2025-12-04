d0b7aa4870b735be7811933d4ea436e22f4d53f4
maaz <mdmaazi2907@gmail.com>
2025-12-04 11:04:42 +0530

    changed color scheme to github dark.

--- .config/nvim/lua/maaz/plugins/colorscheme.lua
+++ .config/nvim/lua/maaz/plugins/colorscheme.lua
@@ -1,10 +1,13 @@
 return {
-	"ellisonleao/gruvbox.nvim",
-	lazy = false,
-	priority = 1000,
-	opts = {},
+	"projekt0n/github-nvim-theme",
+	name = "github-theme",
+	lazy = false, -- make sure we load this during startup if it is your main colorscheme
+	priority = 1000, -- make sure to load this before all the other start plugins
 	config = function()
-		require("gruvbox").setup()
-		vim.cmd.colorscheme("gruvbox")
+		require("github-theme").setup({
+			-- ...
+		})
+
+		vim.cmd("colorscheme github_dark")
 	end,
 }
